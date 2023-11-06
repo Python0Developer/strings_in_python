0def88c572ad05eca416b044fb661b919085e999
Python0Developer <abdulalamri120@yahoo.com>
2023-11-06 08:55:05 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+adj = input('Adj:')
+verb1 = input('verb:')
+noun = input('noun:')
+x = f"What in the hell of the seven kingdoms are these people {verb1} like who are they {noun} and how are they {adj}"
+print(x)
